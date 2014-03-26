95ea3655ace22b27cc11d9dd7476958cbf775211
liluo <i@liluo.org>
2014-03-26 11:37:51 +0800

    Initial Commit

---
+++ Scanner/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Scanner
+//
+//  Created by liluo on 3/26/14.
+//  Copyright (c) 2014 liluo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Scanner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Scanner
+//
+//  Created by liluo on 3/26/14.
+//  Copyright (c) 2014 liluo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
